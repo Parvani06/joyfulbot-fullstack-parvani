aa2d1631fde527215c34ff09ede2fea38be94c74
Parvani <parvanijain2005@gmail.com>
2026-05-30 17:53:51 +0530

    feat: add JWT interceptor, error interceptor and route guards

---
+++ frontend/elms-frontend/src/app/interceptors/auth.interceptor.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable()
+export class AuthInterceptor implements HttpInterceptor {
+
+  constructor(private authService: AuthService) {}
+
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    var token = this.authService.getToken();
+    if (token) {
+      var cloned = request.clone({
+        headers: request.headers.set('Authorization', 'Bearer ' + token)
+      });
+      return next.handle(cloned);
+    }
+    return next.handle(request);
+  }
+}
